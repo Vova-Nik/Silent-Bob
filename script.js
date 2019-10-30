706bf3790eff46a3a35b636400c4e360bd3c8d29
Vova-Nik <33543617+Vova-Nik@users.noreply.github.com>
2019-10-30 21:31:31 +0200

    Home if switc etc

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ console.log("script loaded");
 console.log("-1/0 = " + -1/0);
 
 let myDiv = document.querySelector("#test");
- myDiv.innerHTML = 'nnn ';
+myDiv.innerHTML = 'nnn ';
 myDiv.innerHTML += '\n0';
 //  myDiv.innerHTML += Boolean(null<=0);
 let i = 3;
@@ -13,5 +13,36 @@ function showMessage(from, text = "текст не добавлен") {
     console.log( from + ": " + text );
   }
   
-  showMessage("Аня"); // Аня: текст не добавлен
-  showMessage("Аня", "ghbdtn");
+  // showMessage("ANT"); // Аня: текст не добавлен
+  // showMessage("ANT", "ghbdtn");
+
+  const button = document.querySelector('button');
+  const input = document.querySelector('.age');
+  button.onclick = ()=>{
+    let num = + input.value;
+    console.log(num);
+    if(num >= 16)
+      console.log('welcom');
+    else
+      console.log('Bye');
+  }
+
+  let a = 3;
+
+switch (a) {
+  case 3:
+    {
+      console.log ('Маловато' );
+      console.log ('Маловато1' );
+      break;
+    }
+  case 4:
+    console.log( 'В точку!' );
+    break;
+  case 5:
+    console.log( 'Перебор' );
+    break;
+  default:
+    console.log( "Нет таких значений" );
+    
+}
