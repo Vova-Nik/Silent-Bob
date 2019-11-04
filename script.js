22e164880089eeb187a86722af640b752ba32df9
Vova-Nik <vmaseich@gmail.com>
2019-11-04 06:51:36 +0200

    map + filter metodes passed

--- script.js
+++ script.js
@@ -1,33 +1,58 @@
 console.clear();
 console.log("script loaded");
 
-let btn = document.querySelector('#button1');
+let btnOk = document.querySelector('#button1');
 
-btn.onclick = () => {
-  console.log(`btn on click call point`);
-  parag.classList.add('parag1');
-  obj.objPrintVars();
-}
+const parag = document.querySelector('.lorem');
+const btnChange = document.querySelector('.changeStyleBtn');
 
-let obj = {
-  a: 1,
-  b: 2,
-
-  objPrintVars() {
-    console.log(`objFunc is running a = ${this.a}  b = ${this.b}`);
+btnChange.onclick = () => {
+  console.log(`btnChange pressed`);
+  if (btnChange.getAttribute('data-state') == 0) {
+    parag.classList.add('parag1');
+    btnChange.setAttribute('data-state', '1');
+    console.log(`Button state = ${btnChange.getAttribute('data-state')}`);
+  }
+  else {
+    parag.classList.remove('parag1');
+    btnChange.setAttribute('data-state', '0');
+    console.log(`Button state = ${btnChange.getAttribute('data-state')}`);
   }
 }
 
-console.log(`a = ${this.a}  b = ${this.b}`);
+parag.style.width = '50%';
+const testDiv = document.querySelector('#testDiv');
+//testDiv.innerHTML = 'Hi Vova <br> Ewrth is OK';
 
-function oneFunc() {
-  console.log(`oneFunc is running`);
-}
 
-const parag = document.querySelector('.lorem');
-parag.style.width = '200px';
-parag.style.backgroundcolor = '#000000';
-parag.innerHTML = `1234`;
-parag.classList.add(contenteditable = 'true');
-console.log(parag.style);
+
+const mObject = {
+  'a': 'ad vova',
+  'b': 'bd petya',
+  'c': 'cd alla',
+  'd': 'dd Lalisa'
+};
+
+
+for(let k in mObject){
+  console.log('k = ' + mObject[k]);
+ }
+
+a = new Set();
+a.add('Vova');
+a.add('Peter');
+a.add('Nik');
+console.log('a = ' + a);
+console.log(a);
+
+aa = Array(a);
+console.log(aa);
+
+
+let o = [4, 5, 12, 200, 1, 0, -2];
+let b = o.map(function(item, index){
+  //console.log(`item = ${item}`);
+  return item;
+})
+console.log(o);
 
